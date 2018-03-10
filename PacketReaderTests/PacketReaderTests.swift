2b09f892dbebcca31317cf089bdc77dcf8c58244
Philip Shen <philipshen13@gmail.com>
2018-03-10 21:28:43 +0800

    Initial Commit

---
+++ PacketReaderTests/PacketReaderTests.swift
@@ -0,0 +1,36 @@
+//
+//  PacketReaderTests.swift
+//  PacketReaderTests
+//
+//  Created by Philip Shen on 3/10/18.
+//  Copyright © 2018 Philip Shen. All rights reserved.
+//
+
+import XCTest
+@testable import PacketReader
+
+class PacketReaderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
